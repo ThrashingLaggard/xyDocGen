c8d3a6e6d4ff3228b91af7eea8c9e3b92c1e1b49
Hansi <test@lol.de>
2025-10-26 17:55:59 +0100

    Updated dependencies and their dependencies

--- xyDocGen/Core/Renderer/MarkdownRenderer.cs
+++ xyDocGen/Core/Renderer/MarkdownRenderer.cs
@@ -1,6 +1,4 @@
-﻿using FluentAssertions.Equivalency;
-using OpenQA.Selenium.BiDi.Modules.Log;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
