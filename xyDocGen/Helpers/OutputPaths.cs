fd80e0c0756656a0db272266af89114c7945b9a0
Hansi <test@lol.de>
2025-11-01 00:58:18 +0100

    New class: CLI OptionsParser

--- xyDocGen/Helpers/OutputPaths.cs
+++ xyDocGen/Helpers/OutputPaths.cs
@@ -4,13 +4,10 @@ using System.IO;
 
 internal static class OutputPaths
 {
-    public static string FormatDir(string outRoot, string format) =>
-        Path.Combine(outRoot, format.ToLowerInvariant());
+    public static string FormatDir(string outRoot, string format) =>Path.Combine(outRoot, format.ToLowerInvariant());
 
     // Index/Tree JE FORMAT innerhalb des jeweiligen Format-Ordners
-    public static string IndexPath(string outRoot, string formatExt) =>
-        Path.Combine(FormatDir(outRoot, formatExt), $"index.{formatExt.ToLowerInvariant()}");
+    public static string IndexPath(string outRoot, string formatExt) => Path.Combine(FormatDir(outRoot, formatExt), $"index.{formatExt.ToLowerInvariant()}");
 
-    public static string TreePath(string outRoot, string formatExt) =>
-        Path.Combine(FormatDir(outRoot, formatExt), $"tree.{formatExt.ToLowerInvariant()}");
+    public static string TreePath(string outRoot, string formatExt) => Path.Combine(FormatDir(outRoot, formatExt), $"tree.{formatExt.ToLowerInvariant()}");
 }
