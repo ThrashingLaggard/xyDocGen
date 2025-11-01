01886fe774fdcf3f3a7fa84244e04f11286063dc
Hansi <test@lol.de>
2025-11-01 02:33:45 +0100

    Removed unused project parser

--- xyDocGen/Docs/SyntaxNodeExtensions.cs
+++ xyDocGen/Docs/SyntaxNodeExtensions.cs
@@ -1,7 +1,7 @@
 ﻿using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
 
-namespace xyDocumentor.Parser
+namespace xyDocumentor.Docs
 {
     internal static class SyntaxNodeExtensions
     {
